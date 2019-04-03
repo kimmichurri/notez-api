64a73af7d8fb2c3000080940b15b8d7a8720f2e4
Joshua Lavarine <joshua@lavarine.com>
2019-04-03 16:31:33 -0600

    Create PUT test for 'happy' path
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>
    Co-authored-by: Justin Torrance <justin.torrance91@gmail.com>

--- app.test.js
+++ app.test.js
@@ -13,7 +13,7 @@ describe('/api/v1/notes', () => {
     app.locals.notes = notes;
   })
 
-  describe('get to /notes', () => {
+  describe('GET to /notes', () => {
     it('should return a status of 200 with a notes array', async () => {
       const response = await request(app).get('/api/v1/notes');
       expect(response.status).toBe(200);
@@ -21,16 +21,33 @@ describe('/api/v1/notes', () => {
     })
   })
 
-  describe('get to /notes/:id', () => {
+  describe('GET to /notes/:id', () => {
     it('should return a status of 200 with a note object', async () => {
       const response = await request(app).get('/api/v1/notes/1');
       expect(response.status).toBe(200);
       expect(response.body).toEqual(notes[0]);
     })
     it('should return a status of 404 with a message of "Note does not exist"', async () => {
-      const response = await request(app).get('/api/v1/notes/12345');
+      const response = await request(app).get('/api/v1/notes/1012');
       expect(response.status).toBe(404);
       expect(response.body).toBe('Note does not exist');
+    })
+  })
+
+  describe('PUT to /notes/:id', () => {
+    it('should return a status of 204 and update note object', async () => {
+      expect(app.locals.notes[0]).toEqual(notes[0]);
+
+      shortid.generate = jest.fn().mockImplementation(() => '329')
+      const noteUpdate = {
+        title: 'newTitle', 
+        body: [{ id: shortid.generate(), text: 'newFaketext' }] 
+      }
+      const response = await request(app).put('/api/v1/notes/1')
+        .send(noteUpdate);
+
+      expect(response.status).toBe(204);
+      expect(app.locals.notes[0]).toEqual({ id: notes[0].id, title: 'newTitle', body: [{ id: shortid.generate(), text: 'newFaketext' }] })
     })
   })
 
