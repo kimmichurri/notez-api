d32a8119e8b59cf4a11722e09ccf37b66a262f26
“Kimberly <kimberly.achkio@gmail.com>
2019-04-26 13:43:46 -0600

    Add annotations for imports and app.get

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 import express from 'express';
+//we are importing the express framework so that we can write out our code with a different syntax (considered by many developers to be easier to read and write, and I like it too)
+//notice we aren't using the 'require' syntax- this is because we installed babel and can use ES6 'import'
 const app = express();
+//here we are stating that our app is an express app
 import cors from 'cors';
-import shortid from 'shortid';
 app.use(cors());
+//CORS prevents requests made across domains, but we want to be able to be able to make requests from a front-end that is hosted at another domain so we need to use cors here
+import shortid from 'shortid';
+//this is just a little npm package for generating ids for our data
 app.use(express.json());
+//this ensures that the data is parsed as JSON, which is what we want to be dealing with here
 
 app.locals.notes = [
   { 
-    id: '1', 
+    id: 10,
     title: 'fakeTitle', 
     listItems: [
-      {id: shortid.generate(), text: 'faketext', completed: false}, 
-      {id: shortid.generate(), text: 'faketextb', completed: false}
+      {id: 'a', text: 'faketext', completed: false},
+      {id: 'b', text: 'faketextb', completed: false}
     ]},
   { 
-    id: '2', 
+    id: 11,
     title: 'fakeTitle2', 
     listItems: [
-      {id: shortid.generate(), text: 'faketext2', completed: false}
+      {id: 'c', text: 'faketext2', completed: false}
     ]}
 ];
+//app.locals is a place where we can store data in our backend, kinda like how we can store data in localStorage in front-end- Both are short-term solutions for data storage
+//we gave it a property of notes to store an array of note objects. We could add more properties and store more information of various data types
 
 app.get('/api/v1/notes/', (request, response) => {
+  //app is an object that we assign to be an express application at the top of our file
+  //app.get is the method from Express we use to make our GET request at the API endpoint listed as the first argument
+  //the second argument is a callback function that is called when a request is made to this route- so these methods are sitting and waiting/listening for requests
   response.status(200).json(app.locals.notes);
+  //this sets the status code of the response, which is 200 for a successful reponse
+  //after that we are chaining on the JSON that will be sent back with the response, which will be our array of notes stored as a property on app.locals
+  //.json can also be used to convert data to JSON, which we will see later
 });
+
 
 app.get('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
